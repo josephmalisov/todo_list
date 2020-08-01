8bd599520fce5be6100085b8b3b2b55cad009b64
Joseph Malisov <j.a.malisov@gmail.com>
2020-07-31 22:52:51 -0400

    changes to models.py and added deploy script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+export DATABASE_URL=postgres://lztrxinohmzanu:784d7848b47b8c8ed9a55d4223644912fcfc87fbb9bdd8bc45abac9a8c75e017@ec2-54-211-210-149.compute-1.amazonaws.com:5432/d8trkkj3bm36c9
+python app.py
