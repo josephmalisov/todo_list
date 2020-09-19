b78eb8a1046f1acb78f217c527d4de0ada2216e1
Joseph Malisov <j.a.malisov@gmail.com>
2020-09-19 00:14:34 -0400

    created button to copy link

--- static/js/app.js
+++ static/js/app.js
@@ -174,5 +174,17 @@ function search() {
     renderAll(found)
 }
 
+function copylink() {
+
+    var dummy = document.createElement('input'),
+        text = window.location.href;
+
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+}
+
 //fetch notes
 updateResults();
